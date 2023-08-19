5da04f7a553ccba5c0f8d678fc9b44b62cd31850
Alex Kernot <alex.kernot04@gmail.com>
2023-08-19 09:52:41 +0930

    Add simple console IO

---
+++ Console.cpp
@@ -0,0 +1,18 @@
+#include "Console.hpp"
+
+#include <iostream>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+std::string Console::sendInput(void) {
+	string input;
+	cin >> input;
+	return (input);
+}
+
+void Console::displayOutput(std::string output) {
+	cout << output << endl;
+}
