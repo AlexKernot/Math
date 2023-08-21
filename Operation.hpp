27e10e3a0914665997b653a18baaeff4f1640c15
Alex Kernot <alex.kernot04@gmail.com>
2023-08-21 17:30:50 +0930

    Feat: Add Operation class

---
+++ Operation.hpp
@@ -0,0 +1,15 @@
+#ifndef OPERATION_HPP
+# define OPERATION_HPP
+
+# include "Token.hpp"
+
+class Operation : public Token {
+private:
+	int precedence;
+public:
+	Operation(int _precedence) : Token(operation), precedence(_precedence) {}
+	virtual Token operate(Token& a, Token& b) = 0;
+	int getPrecedence() {return precedence;}
+};
+
+#endif // OPERATION_HPP
