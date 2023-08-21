103e267fb39d057ecdae1ed7b9675dee6459e7ae
Alex Kernot <alex.kernot04@gmail.com>
2023-08-21 17:30:01 +0930

    Refactor: Change Process method return type

--- PostFixProcessor.hpp
+++ PostFixProcessor.hpp
@@ -5,9 +5,19 @@
 
 # include <stack>
 # include <queue>
+# include <memory>
 
 class PostFixProcessor {
-	static std::queue<Token &> process(std::queue<Token &>& input);
+public:
+    // Uses a shunting yard algorithm to transform a queue of Tokens in
+    // infix notation into postfix or reverse Polish notation.
+    static std::unique_ptr<std::queue<Token &>> process(
+        std::queue<Token &>& input);
+private:
+    static void pushOperators(Token& current, std::queue<Token&>& returnQueue,
+                              std::stack<Token&> &tempStack);
+    static void pushUntilBrackets(std::queue<Token&>& returnQueue,
+                                  std::stack<Token&>& tempStack);
 };
 
 #endif // POSTFIXPROCESSOR_H
