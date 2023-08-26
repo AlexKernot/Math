d7ced3eb0bcc89125393bac5a85003237a270c32
Alex Kernot <alex.kernot04@gmail.com>
2023-08-26 18:41:45 +0930

    Chore: clean root directory

---
+++ src/Tokenizer.cpp
@@ -0,0 +1,9 @@
+#include "Tokenizer.hpp"
+#include <ctype.h>
+
+std::queue<Token *> *Tokenizer::process(std::string input) {
+	std::queue<Token *> *returnQueue = new std::queue<Token *>();
+	for (int i = 0; i < input.length(); ++i) {
+
+	}
+}
